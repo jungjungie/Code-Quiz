6f460ab22c2cf50a6634bf7a397d2fbf5042f5b9
Esther Min <estherjmin@gmail.com>
2020-06-08 17:46:11 -0400

    Upon start, hidden ol shows and h1 hides

--- script.js
+++ script.js
@@ -3,55 +3,65 @@ var startBtn = document.querySelector("#startBtn");
 
 // Array of Q&A
 var questionArr = [
-    {q1: "Which one of these is not a JavaScript data type?",
-    choice1: "boolean",
-    choice2: "var",
-    choice3: "string",
-    choice4: "undefined",
-    answer: 2
+    {q: "Which one of these is not a JavaScript data type?",
+    choices: ["boolean", "var", "string", "undefined"],
+    answer: "var"
     },
-    {q2: "What does 'DOM' stand for?",
-    choice1: "Document Object Model",
-    choice2: "Direct Object Model",
-    choice3: "Div Object Main",
-    choice4: "Done Object Modeling",
-    answer: 1 
+    {q: "What does 'DOM' stand for?",
+    choices: ["Document Object Model", "Direct Object Model", "Div Object Main", "Done Object Modeling"],
+    answer: "Document Object Model"
     },
-    {q3: "What does 'this' keyword refer to in JavaScript?",
-    choice1: "The current variable",
-    choice2: "The current website",
-    choice3: "The current function",
-    choice4: "The current object",
-    answer: 4
+    {q: "What does 'this' keyword refer to in JavaScript?",
+    choices: ["The current variable", "The current website", "The current function", "The current object"],
+    answer: "The current object"
     }
 ]
 
+// console.log(questionArr[0].q1);
 // Function to loop through Q&A array
 function runQuiz() {
 
-// make h1 disappear
-// add p to display question
-// add ol with li to display multiple choices
-// create for loop to run through the Q&A array
-// create for loop that will add each choice to each li
-// create if statement to determine correct & incorrect answers
-        // if incorrect, timer deducts 5 seconds & goes to next q&a
-        // if correct, no impact to timer & go to next q&a
-// when a multiple choice is clicked, replace the p and ol/li with next Q&A choices
+    var questionIndex = -1;
 
+    // h1 disappears
+    document.querySelector("h1").style.display = "none";
 
-var currentTime = 75;
+    // Shows ol & li for multiple choice
+    document.querySelector("#multipleChoice").style.display = "block";
+    
+    // #changingTxt displays question
+    // if(questionIndex < questionArr.length) {
+    //     questionIndex = questionIndex + 1;
+    //     document.querySelector("#changingTxt").textContent = questionArr[questionIndex].q;
+    //     console.log(questionArr[questionIndex].q)
+    // }
 
-// When start button is clicked, timer starts to countdown
-var timer = setInterval(function() {
-    if (currentTime > 0) {
-        currentTime--; 
-        document.querySelector("#countdown").textContent = currentTime;
+    for (var i=0; i < questionArr.length; i++) {
+        document.querySelector("#changingTxt").textContent = questionArr[i].q;
+        console.log(questionArr[i].q)
     }
-}, 1000);
+     
+    // add ol with li to display multiple choices
+    // create for loop to run through the Q&A array
+    // create for loop that will add each choice to each li
+    // create if statement to determine correct & incorrect answers
+            // if incorrect, timer deducts 5 seconds & goes to next q&a
+            // if correct, no impact to timer & go to next q&a
+    // when a multiple choice is clicked, replace the p and ol/li with next Q&A choices
+
+
+    var currentTime = 75;
+
+    // When start button is clicked, timer starts to countdown
+    var timer = setInterval(function() {
+        if (currentTime > 0) {
+            currentTime--; 
+            document.querySelector("#countdown").textContent = currentTime;
+        }
+    }, 1000);
 
 }
-var questionIndex = 0;
+
 
 
 
