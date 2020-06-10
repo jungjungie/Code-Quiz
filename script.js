18b670fc5844510568242bd8844e57f1dfedf0c5
Esther Min <estherjmin@gmail.com>
2020-06-09 20:06:16 -0400

    Fixed timer and added more Q&A

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var questionIndex = -1;
 var score = 0;
 var mcBtnArr = document.querySelectorAll(".mcBtns");
 var result = document.querySelector("#correctOrWrong");
+var timer = null;
 
 // Array of Q&A
 var questionArr = [
@@ -28,19 +29,34 @@ var questionArr = [
         q: "What does 'this' keyword refer to in JavaScript?",
         choices: ["The current variable", "The current website", "The current function", "The current object"],
         answer: "The current object"
+    },
+    {
+        q: "Which of these does not mean the same thing as the others?",
+        choices: ["count + 1", "count = count + 1", "count += 1", "count++"],
+        answer: "count + 1"
+    },
+    {
+        q: "__________ is used to select an element and assign it to a variable.",
+        choices: [".GetThisElement()", ".getElementId()", ".setElement", ".querySelector()"],
+        answer: ".querySelector()"
+    },
+    {
+        q: "If you have an array of letters A through D, which letter is at index 1?",
+        choices: ["A", "B", "C", "D"],
+        answer: "B"
     }
 ]
 
-// Function to start quiz
-function runQuiz() {
+// Function to start countdown
+function runTimer() {
 
-    // Timer starts to countdown
-    var timer = setInterval(function() {
-        if (currentTime > 0) {
+    if (timer == null) {
+        timer = setInterval(function() {
             currentTime--; 
             document.querySelector("#countdown").textContent = currentTime;
-        }
-    }, 1000);
+        }, 1000);
+    }
+    // add else statement to clearInterval(timer)
 
     // h1 disappears
     document.querySelector("h1").style.display = "none";
@@ -72,7 +88,7 @@ function populateQ() {
 function checkAnswer(event) {
     // If answer is correct, add to score & go to next Q&A
     if (event.target.textContent == questionArr[questionIndex].answer) {
-        score++;
+        score += 10;
         document.querySelector("#score").textContent = score; 
 
         result.textContent = "Great job! That's correct!";
@@ -87,7 +103,9 @@ function checkAnswer(event) {
         populateQ();
     }
 
+    // Shows commentary
     result.style.display = "block";
+    // Hides commentary after 1.5 seconds
     setTimeout(function() {
         result.style.display = "none";
      }, 1500);
@@ -104,7 +122,7 @@ function hideBtn() {
 }
 
 // Quiz starts to run when Start Quiz Button is clicked
-startBtn.addEventListener("click",runQuiz)
+startBtn.addEventListener("click",runTimer)
 
 // Adding event to multiple choice buttons
 for (var i=0; i < mcBtnArr.length; i++) {
@@ -116,7 +134,6 @@ for (var i=0; i < mcBtnArr.length; i++) {
 
 
 
-// Need to add text popup for "Correct!" and "Try Again!"
 // Add more to Q&A queue
 // Create high score page & link to it when time is up or when all Qs answered
 // When game is over, allow user to save initials and score
